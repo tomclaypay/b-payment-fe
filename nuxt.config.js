8c526ba8d87ea445bef6c33011e55322ff736e3a
tomclaypay <148337565+tomclaypay@users.noreply.github.com>
2024-11-08 17:04:32 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,51 +1,44 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Payment',
+    title: "EPAY",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
 
   css: [
-    '@/assets/css/main.css',
-    '@/assets/css/app.scss',
+    "@/assets/css/main.css",
+    "@/assets/css/app.scss",
     "@/assets/css/element-ui.scss",
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '@/plugins/vue-notification', mode: 'client' },
-    { src: "~/plugins/element-ui" }
+    { src: "@/plugins/vue-notification", mode: "client" },
+    { src: "~/plugins/element-ui" },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxt/postcss8'
-  ],
+  buildModules: ["@nuxt/postcss8"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/dotenv',
-    '@nuxtjs/axios'
-  ],
+  modules: ["@nuxtjs/dotenv", "@nuxtjs/axios"],
 
   axios: {
-    baseURL: process.env.API_URL
+    baseURL: process.env.API_URL,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
@@ -60,5 +53,5 @@ export default {
   env: {
     apiUrl: process.env.API_URL,
     publicKey: process.env.PUPLIC_KEY,
-  }
-}
+  },
+};
